9788f0c10541811d558f220065d41156e4d1ac8c
Leonid Rezvitsky <rezvitsky@swert.ru>
2022-01-22 12:01:11 +0700

    Update index.ts

--- src/apollo/index.ts
+++ src/apollo/index.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-    uri: <string> import.meta.env.VITE_GRAPHQL,
+    uri: import.meta.env.VITE_GRAPHQL as string,
     cache: new InMemoryCache()
 })
 
